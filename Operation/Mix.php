3590dd1d25cf756c54ae3c0300b52a1d1c8499a4
bptakuma <omomotakuma@gmail.com>
2023-02-28 11:04:30 +0900

    バリデート機能のバグ修正

--- Operation/Mix.php
+++ Operation/Mix.php
@@ -6,36 +6,38 @@ use Validator\Validate;
 
 class Mix
 {
+  public $meat_gram;
+  public $egg_gram;
+  public $bread_gram;
+  public $salt_gram;
+  public $all;
+
   public function mix($onion_gram){
     echo "「肉を何グラム使用しますか？」\n";
-    $meat_gram = 0;
-    $meat_gram = Validate::validate($meat_gram);
+    $this->meat_gram += Validate::validate();
 
     echo "「卵を何グラム入れますか？」\n";
-    $egg_gram = 0;
-    $egg_gram = Validate::validate_notice($egg_gram, "卵", $meat_gram, 3);
+    $this->egg_gram += Validate::validate_notice("卵", $this->meat_gram, 3);
 
     echo "「パン粉を何グラム入れますか？」\n";
-    $bread_gram = 0;
-    $bread_gram = Validate::validate_notice($bread_gram, "パン粉", $meat_gram, 5);
+    $this->bread_gram += Validate::validate_notice("パン粉", $this->meat_gram, 5);
 
     echo "「塩を何グラム入れますか？」\n";
-    $salt_gram = 0;
-    $salt_gram = Validate::validate_notice($salt_gram, "塩", $meat_gram, 15);
+    $this->salt_gram += Validate::validate_notice("塩", $this->meat_gram, 15);
 
-    $all = $meat_gram + $egg_gram + $bread_gram + $onion_gram;
+    $this->all = $this->meat_gram + $this->egg_gram + $this->bread_gram + $onion_gram;
 
-    echo "「全体重量{$all}グラムに対して塩を{$salt_gram}グラム入れました」\n";
+    echo "「全体重量{$this->all}グラムに対して塩を{$this->salt_gram}グラム入れました」\n";
     
-    $flag = $salt_gram / $all;
+    $flag = $this->salt_gram / $this->all;
     $getpoint = 0;
 
     if (0.005 <= $flag && $flag <= 0.015){
       $getpoint += 2;
-      return array($getpoint, $all);
+      return array($getpoint, $this->all);
     } else {
       $getpoint -= 2;
-      return array($getpoint, $all);
+      return array($getpoint, $this->all);
     }
     
   }
